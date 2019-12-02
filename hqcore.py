6ef268e2e4c57c5a5f21b7ba615afffaec65e0f6
markjadams <markjadams@live.com>
2019-12-02 21:03:00 +0000

    added print_file

--- hqcore.py
+++ hqcore.py
@@ -6,10 +6,13 @@
 import random
 
 def rollDice():
-	return random.randint(1, 6)
+    return random.randint(1, 6)
 
 def intToBool(oneZero):
-	return True if oneZero == 1 else False
+    return True if oneZero == 1 else False
 
 def boolToStr(val):
-	return "X" if val == True else "."
+    return "X" if val == True else "."
+
+def print_file(file):
+    print open(file, "r").read()
